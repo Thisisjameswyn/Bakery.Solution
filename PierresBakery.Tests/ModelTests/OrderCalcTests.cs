90ea43a5fd78b4e058c84283e4bdabf4b134e668
James Wyn <thisisjameswyn@gmail.com>
2021-05-17 14:32:32 -0700

    add final test

--- PierresBakery.Tests/ModelTests/OrderCalcTests.cs
+++ PierresBakery.Tests/ModelTests/OrderCalcTests.cs
@@ -7,15 +7,27 @@ namespace PierresBakery.Tests
   public class OrderCalcTests
   {
     [TestMethod]
-    public void CostPush_ReturnOrderAmount_OrderCalc()
+    public void CostPush_ReturnNoDeal_OrderCalc()
     {
       OrderSetter testOrder = new OrderSetter();
-      testOrder.BreadAmount = 3;
-      testOrder.PastryAmount = 3;
+      testOrder.BreadAmount = 2;
+      testOrder.PastryAmount = 2;
       testOrder.PushOrder("Bread");
       testOrder.PushOrder("Pastry");
       Assert.AreEqual(testOrder.BreadCost, 10);
-      Assert.AreEqual(testOrder.PastryCost, 5);
+      Assert.AreEqual(testOrder.PastryCost, 4);
+    }
+
+    [TestMethod]
+    public void CostPush_ReturnDeal_OrderCalc()
+    {
+      OrderSetter testOrder = new OrderSetter();
+      testOrder.BreadAmount = 6;
+      testOrder.PastryAmount = 6;
+      testOrder.PushOrder("Bread");
+      testOrder.PushOrder("Pastry");
+      Assert.AreEqual(testOrder.BreadCost, 20);
+      Assert.AreEqual(testOrder.PastryCost, 10);
     }
   }
 }
